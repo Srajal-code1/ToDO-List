928a2a97917e3a00172c0e3b3fb6d7e9efd26a52
Srajal-code1 <srajalasthana78@gmail.com>
2026-03-08 13:18:19 +0530

    updated code

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
 
-let ctr = 0;
-function callback() {
-    console.log(ctr);
-    ctr = ctr + 1;
-}
+// let ctr = 0;
+// function callback() {
+//     console.log(ctr);
+//     ctr = ctr + 1;
+// }
 
-setInterval(callback, 2000);
+// setInterval(callback, 2000);
+
+
+function deleteTodo(index) {
+    const element = document.getElementById("todo-" + index);
+    element.parentNode.removeChild(element);
+
+}
